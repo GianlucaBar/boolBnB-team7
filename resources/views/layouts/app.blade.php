b66ff1041b995b7829db19a9f348f8ae9e0366dc
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-07-15 18:07:14 +0200

    added logo in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,16 +29,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
                 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    <div class="logo">
+                        <img src={{asset('img/logo.png')}} alt="Logo">
+                    </div>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
