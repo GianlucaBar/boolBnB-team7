40f795c34ef262161f0ffba8e20938a0d2f8fba8
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-07-25 01:06:32 +0200

    Thank you page styled

--- resources/views/admin/thankyou.blade.php
+++ resources/views/admin/thankyou.blade.php
@@ -5,8 +5,17 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <h2>Grazie per il tuo acquisto!</h2>
-    <div>id transazione: {{ $transactionId }}</div>
+<div class="thank-you-container">
+    <div class="container-fluid">
+        <h2>Grazie per il tuo acquisto!</h2><div class="transaction">
+            Troverai il tuo appartamento nella sezione in evidenza
+        </div>
+        <div class="transaction"><span class="strong-text">id transazione:</span> {{ $transactionId }}</div>
+        <div>
+            <a class="btn" href="{{ url('/') }}">
+                Torna alla home
+            </a>
+        </div>
+    </div>
 </div>
 @endsection
