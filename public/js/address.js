8fb1486de15544409dac08bda0ca4a5e4fd4d592
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-07-24 23:51:52 +0200

    Dashboard is fully responsive

--- public/js/address.js
+++ public/js/address.js
@@ -934,7 +934,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Gianluca\laravel-projects\progetto finale\boolBnB-team7\resources\js\address.js */"./resources/js/address.js");
+module.exports = __webpack_require__(/*! C:\Users\assas\Boolean\progetto_finale\boolBnB-team7\resources\js\address.js */"./resources/js/address.js");
 
 
 /***/ })
--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -14,20 +14,31 @@
     
     <div class="container">
         <div class="title centered mt-4 mb-4">
-            <h2><i class="fas fa-digital-tachograph"></i> DASHBOARD</h2>
+            <h2>LA TUA DASHBOARD</h2>
         </div>
 
         <div class="container-user ">
             <div class="user-rep ">
-                <h2><i class="fas fa-user-tie"></i> I Tuoi Dati <i class="fas fa-user-tie"></i></h2>
-                <div>Nome Utente: {{ $user->name }}</div>
-                <div>Email: {{ $user->email }}</div>
-                <div>Numero Appartamenti: {{ $apartment_number}}</div>
+                <h2><i class="fas fa-user"></i> I Tuoi Dati <i class="fas fa-user icon-hide"></i></h2>
+                <div class="user-rep-box">
+                    <div>
+                        <h4>Nome Utente</h4>
+                        {{ $user->name }}
+                    </div>
+                {{-- <div>
+                    <h4>Email</h4> 
+                    {{ $user->email }}
+                </div> --}}
+                    <div>
+                        <h4>Numero Appartamenti</h4>
+                        {{ $apartment_number}}
+                    </div>
+                </div>
             </div>
 
             {{-- Messages --}}
             <div class="msg-box">
-                <h2> <i class="far fa-envelope"></i> I Tuoi Messaggi <i class="far fa-envelope"></i></h2>
+                <h2> <i class="far fa-envelope"></i> I Tuoi Messaggi <i class="far fa-envelope icon-hide"></i></h2>
                 @foreach ($messagesArray as $messages)
                     @foreach ($messages as $message)
                     <div>
@@ -53,24 +64,25 @@
     <div class="container">
 
         <div class="my-ap mt-4 mb-2 centered">
-            <h2>I Tuoi Appartamenti</h2>
+            <h2 class="big-h2">I Tuoi Appartamenti</h2>
         </div>
 
         {{-- Add new Apartment --}}
         <div class="new-ap centered ">
-            <a id="new-ap"  href="{{ route('admin.apartments.create') }}"><i class="fas fa-home"></i> Aggiungi appartamento </a>
+            <a id="new-ap"  href="{{ route('admin.apartments.create') }}"><i class="fas fa-plus"></i> Aggiungi appartamento </a>
         </div>
 
         <div class="dashboard-ap mt-4 mb-4">
             @foreach ($apartments as $apartment)
             <div class="ap-line">
-                <h5><a class="btn-link" href="{{ route('admin.apartments.show', ['apartment' => $apartment->id]) }}">{{$apartment->title}}</a></h5>
+                <a class="btn-link" href="{{ route('admin.apartments.show', ['apartment' => $apartment->id]) }}">{{$apartment->title}}</a>
 
                 {{-- CRUD --}}
                 <div class="buttoni">
 
                     <a class="btn-modify" href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}">
-                        Modifica <i class="fas fa-wrench"></i>
+                        Modifica
+                        {{-- Modifica <i class="fas fa-wrench"></i> --}}
                     </a>
 
                     <form  action="{{ route('admin.apartments.destroy', [
@@ -85,7 +97,8 @@
                     </form>
 
                     <a class="btn-sponsor" href="{{ route('admin.sponsor-page', ['id' => $apartment->id]) }}">
-                        Sponsorizza <i class="fas fa-wallet"></i>
+                        Sponsorizza
+                        {{-- Sponsorizza <i class="fas fa-star"></i> --}}
                     </a>
                     
                 </div>
