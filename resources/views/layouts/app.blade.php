c909dac8b9c2ea82adc0a58ce3640e19ff4faf88
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-07-26 10:03:33 +0200

    Filter button in home styled

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,7 @@
                                     {{ __('Logout') }}
                                 </a>
 
-                                <form id="logout-form-1" action="{{ route('logout') }}" method="POST" class="d-none">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
                             </div>
